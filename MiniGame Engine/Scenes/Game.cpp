df3ecf29f9bb37c3c8ad8544d1d923870c51f16d
WoWPerroDev <carlo.relion@saei.mx>
2020-03-20 12:14:04 -0600

    CurrentProject

--- MiniGame Engine/Scenes/Game.cpp
+++ MiniGame Engine/Scenes/Game.cpp
@@ -4,6 +4,7 @@
 #include "..\Log\Console.h"
 #include "lua.h"
 #include "../ScriptingSystem/LuaScript.h"
+#include "../ScriptingSystem/LuaContext.hpp"
 
 ListaT<int> extractIntegerWords(std::string str)
 {
@@ -51,6 +52,10 @@ void Game::Init(Platform* platform, GameStateManager* manager)
 	/*LuaScript script("C:\\Dev_WoWperro\\source\\repos\\7_Trimestre\\Diseño de Sistemas de juegos\\MiniGame Engine\\Debug\\Scripts\\Test.lua");
 	std::string filename = script.get<std::string>("player.filename");
 	int posX = script.get<int>("player.pos.X");*/
+	LuaContext lua;
+	lua.writeVariable("x", 5);
+	lua.executeCode("x = x + 2;");
+	std::cout << lua.readVariable<int>("x") << std::endl;       // prints 7
 }
 
 void Game::Draw()
